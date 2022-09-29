f857ce2bfcf6457f178f6c4319f47f1bd847509c
NatalliaPahosava <mulato4ka114@gmail.com>
2022-09-28 20:37:57 -0400

    Readme.md file added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
       <footer>
-        <a href="https://github.com/NatalliaPahosava/cookies-frontend">Github source frontend</a>
-        <a href="https://github.com/NatalliaPahosava/newcookies-backend">Github source backend</a>
+        <a href="https://github.com/NatalliaPahosava/aws3-fullstack-frontend/tree/main/src/pages">Github source frontend</a>
+        <a href="https://github.com/NatalliaPahosava/c2-fullstack-backend">Github source backend</a>
         <small>Copyrights © 2022 | all rights reserved</small>
       </footer>
     )
